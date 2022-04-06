1a706cb8a68ac9261dc9b7d81fc1c399858dda0c
Xindi Xu <xindixu0@gmail.com>
2022-04-06 16:07:15 -0400

    fix: syntax error for lambda function

--- LF1/lambda_function.py
+++ LF1/lambda_function.py
@@ -1,22 +1,22 @@
 import json
 
-
 ITEMS = [
-    {pid: 1, name: "Kitty 1" },
-    {pid: 2, name: "Kitty 2" },
-    {pid: 3, name: "Kitty 3" },
-    {pid: 4, name: "Kitty 4" },
+    {"pid": 1, "name": "Kitty 1"},
+    {"pid": 2, "name": "Kitty 2"},
+    {"pid": 3, "name": "Kitty 3"},
+    {"pid": 4, "name": "Kitty 4"},
 ]
+
 
 def lambda_handler(event, context):
     # TODO implement search function
-    
+
     print(event)
-    
+
     return {
         'statusCode': 200,
         'body': {
-            count: len(ITEMS)
-            items: ITEMS
+            "count": len(ITEMS),
+            "items": ITEMS
         }
     }
