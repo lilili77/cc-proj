d646a453f54cbdadaf900153d7f623216819f790
lilili77 <lsjohnli7@gmail.com>
2022-04-16 14:47:03 -0400

    feat: LF0 sagemaker invoke endpoint

--- LF0/lambda_function.py
+++ LF0/lambda_function.py
@@ -5,21 +5,26 @@ import base64
 
 def lambda_handler(event, context):
     # TODO implement LF0
-    print(event)
+    
+    # Parameters
     imgBucket = os.environ.get('ImgBucket')
     img_key = event['pathParameters']['imgkey']
-
-    # Alternative way but ContentType is not jpeg
-    # img_data = event['body'].encode('ascii')
-    # with open("/tmp/imageToSave.jpg", "wb") as fh:
-    #     fh.write(base64.decodebytes(img_data))
-    # s3 = boto3.client('s3')
-    # s3.upload_file('/tmp/imageToSave.jpg', imgBucket, img_key, ExtraArgs={'Metadata': {'Content-Type': 'image/jpeg'}})
-
-    img_data = event['body']
-    img_data = base64.b64decode(img_data)
+    img_data = base64.b64decode(event['body'])
+    
+    # Upload img_key,img_data to s3 bucket imgBucket
     s3 = boto3.resource('s3')
     s3.Bucket(imgBucket).put_object(Key=img_key, Body=img_data, ContentType='image/jpeg')
+    
+    # Sagemaker Img to embedding
+    runtime= boto3.client('runtime.sagemaker')
+    payload = json.dumps("{" + "" + "}") # img url
+    response = runtime.invoke_endpoint(EndpointName= os.environ['PredictEndPoint'],
+                                       ContentType='text/csv',
+                                       Body=payload)
+    response = json.loads(response['Body'].read().decode())
+    print(response)
+    
+    # Todo Opensearch k-NN search, get corresponding item title from db
 
     return {
         'statusCode': 200,
