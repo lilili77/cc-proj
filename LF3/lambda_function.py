e0831e37c548614f264100f56f0713086650188a
lilili77 <lsjohnli7@gmail.com>
2022-04-17 14:33:34 -0400

    feat: Create LF3 LF4

---
+++ LF3/lambda_function.py
@@ -0,0 +1,11 @@
+import requests
+import os
+import boto3
+
+def lambda_handler(event, context):
+    # TODO: Return search history
+
+    return {
+        'statusCode': 200,
+        'body': "Hello"
+    }
