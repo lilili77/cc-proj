486c9921943e8ed596d9fc1669513d86c0d4ea51
Xindi Xu <xindixu0@gmail.com>
2022-04-19 01:01:30 -0400

    fix: forgot to build

--- LF5/lambda_function.py
+++ LF5/lambda_function.py
@@ -24,6 +24,47 @@ def validate(params):
     return errors, parsedParams
 
 
+def parse_item(item):
+    created = item['created']['S']
+    image = item['image']['S']
+    link = item['link']['S']
+    name = item['name']['S']
+    price = item['price']['N']
+    retailer = item['retailer']['S']
+    id = item['id']['S']
+
+    return {
+        "id": id,
+        "created": created,
+        "image": image,
+        "link": link,
+        "name": name,
+        "price": price,
+        "retailer": retailer,
+    }
+
+
+def get_product_detail(pid):
+    response = dynamodb.get_item(
+        TableName=PRODUCT_TABLE,
+        Key={
+            'id': {
+                'S': pid
+            }
+        }
+    )
+
+    if "Item" not in response:
+        return None
+
+    print(response)
+    return parse_item(response["Item"])
+
+
+def get_price_histry(pid):
+    pass
+
+
 def lambda_handler(event, context):
     errors, parsedParams = validate(event)
     if len(errors.keys()) > 0:
@@ -35,13 +76,12 @@ def lambda_handler(event, context):
     pid = parsedParams["pid"]
     print('pid', pid)
 
+    detail = get_product_detail(pid)
+
     return {
         'statusCode': 200,
         'body': {
-            'product': {
-                'id': pid,
-                'name': 'yes',
-            },
+            'product': detail,
             'price_history': []
         }
     }
