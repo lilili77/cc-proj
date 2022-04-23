7f17814c15e7a9b5424a9baf0f6bc67510262572
lilili77 <lsjohnli7@gmail.com>
2022-04-23 00:41:41 -0400

    feat: LF3 get search hist

--- LF3/lambda_function.py
+++ LF3/lambda_function.py
@@ -2,8 +2,32 @@ import requests
 import os
 import boto3
 
+
+SearchHistoryTable = os.environ.get('SearchHistoryTable')
+dynamodb = boto3.client('dynamodb')
+
 def lambda_handler(event, context):
     # TODO: Return search history
+
+    # Parameter
+    uid = '10356ed1-d26b-41f3-b940-5111ecc194de'
+    
+    response = dynamodb.query(
+        TableName=SearchHistoryTable,
+        KeyConditionExpression='uid = :v1',
+        ExpressionAttributeValues={
+            ':v1': {
+                'S': uid
+            }
+        }
+    )
+    
+    # list of tuple (q,datetime,imgKey)
+    searchHistory = []
+    for item in response['Items']:
+        searchHistory.append((item['q']['S'],item['datetime']['S'],item['imgKey']['S']))
+    print(searchHistory)
+        
 
     return {
         'statusCode': 200,
