271e48880bae34ac9cb1544b0b341c37d39fac68
Xindi Xu <xindixu0@gmail.com>
2022-05-07 21:10:05 -0400

    fix: update lambda response shape and template

--- LF3/lambda_function.py
+++ LF3/lambda_function.py
@@ -1,4 +1,3 @@
-import requests
 import os
 import boto3
 
@@ -6,7 +5,34 @@ import boto3
 SearchHistoryTable = os.environ.get('SearchHistoryTable')
 dynamodb = boto3.client('dynamodb')
 
-def get_search_hist(uid):
+VALID_METHODS = ["GET", "SEARCH"]
+
+
+def validate(params):
+    uid = params["uid"]
+    method = params["method"]
+
+    errors = {}
+    parsedParams = {
+        "uid": uid,
+        "method": method,
+    }
+
+    if method not in VALID_METHODS:
+        errors["method"] = f"'method' must be one of {VALID_METHODS}"
+    if uid == "":
+        errors["uid"] = "'uid' can't be empty"
+
+    if method == "SEARCH":
+        query = params["q"]
+        parsedParams["q"] = query
+        if query == "":
+            errors["q"] = "'q' can't be empty"
+
+    return errors, parsedParams
+
+
+def get_search_history(uid):
     response = dynamodb.query(
         TableName=SearchHistoryTable,
         KeyConditionExpression='uid = :v1',
@@ -16,20 +42,22 @@ def get_search_hist(uid):
             }
         }
     )
-    
+
     # searchHistory is a list of tuple (q,datetime,imgKey)
     searchHistory = []
     for item in response['Items']:
-        searchHistory.append((item['q']['S'],item['datetime']['S'],item['img']['S']))
-    print(searchHistory)
-    
+        searchHistory.append(
+            (item['q']['S'], item['datetime']['S'], item['img']['S']))
+
     return {
         'statusCode': 200,
-        'items': searchHistory
+        'body': {
+            "items": searchHistory
+        }
     }
 
 
-def search_search_hist(uid,q):
+def search_search_history(uid, q):
     EMPTY = {
         'statusCode': 200,
         'body': {
@@ -46,42 +74,40 @@ def search_search_hist(uid,q):
             }
         }
     )
-    
-    print("response",response)
 
     if "Items" not in response or len(response["Items"]) == 0:
         return EMPTY
-        
+
     items = response["Items"]
     filtered_items = []
     for item in items:
         if q.lower() in item['q']['S'].lower():
             filtered_items.append(item)
-    print("filtered_items",filtered_items)
-    
+
     if not filtered_items:
         return EMPTY
-    
+
     return {
         'statusCode': 200,
         'body': {
             "items": filtered_items
         }
     }
-    
+
 
 def lambda_handler(event, context):
+    errors, parsedParams = validate(event)
+    if len(errors.keys()) > 0:
+        return {
+            'statusCode': 400,
+            'errors': errors
+        }
 
-    print("event", event)
-    uid = event['uid']
-    method = event['method']
+    uid = parsedParams["uid"]
+    method = parsedParams["method"]
 
-    if method == "GET_HISTORY":
-        print('in GET_HISTORY')
-        # Get search history
-        return get_search_hist(uid)
+    if method == "GET":
+        return get_search_history(uid)
     elif method == "SEARCH":
-        # Search search history
-        print('in SEARCH')
-        q = event['q']
-        return search_search_hist(uid,q)
+        query = parsedParams["q"]
+        return search_search_history(uid, query)
